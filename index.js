152e9c5c4d7ce5ad2481eebab5bfd6713747bbd4
Ben Singer <hello@bensinger.me>
2022-07-10 20:27:15 +0100

    rremoved double .js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const base = require('./sds-dsm-configs/base.js');
-const greenJobs = require('./sds-dsm-configs/greenjobs.js');
-const myWow = require('./sds-dsm-configs/mywow.js');
+const base = require('./sds-property-configs/base.js');
+const greenJobs = require('./sds-property-configs/greenjobs.js');
+const myWow = require('./sds-property-configs/mywow.js');
 
 module.exports = {
   base,
