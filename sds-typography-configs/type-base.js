c0d1e5e71c8c902bae7b4a1ea7942fbb55968419
Ben Singer <ben.singer@sds.co.uk>
2022-09-15 12:41:55 +0100

    updating typebase

--- sds-typography-configs/type-base.js
+++ sds-typography-configs/type-base.js
@@ -3,7 +3,7 @@ import {addBase, config} from 'tailwindcss';
 const type = 
     plugin(function({ addBase, config }) {
         addBase({
-          'h1': { fontSize: '30px' },
+          'h1': { fontSize: '60px' },
           'h2': { fontSize: config('theme.fontSize.xl') },
           'h3': { fontSize: config('theme.fontSize.lg') },
         })
